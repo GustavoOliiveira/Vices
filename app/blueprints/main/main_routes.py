3a69a515e19e7b65810b76af0c72b7f2f0fe8f2e
Caetano Smaniotto <smaniottocaetano@gmail.com>
2024-10-19 21:51:12 -0300

    add routes.txt

--- app/blueprints/main/main_routes.py
+++ app/blueprints/main/main_routes.py
@@ -4,3 +4,14 @@ from app.blueprints.main import main_bp
 @main_bp.route("/")
 def main():
     return render_template('InicialPageFiles/inicialPage.html')
+
+@main_bp.route("/central")
+def central():
+    return render_template('CentralPageFiles/index.html')
+
+"""
+@main_bp.route("/nome_da_rota")
+def nome_da_funcao():
+    return render_template('pasta/arquivo') #caminho
+
+"""
